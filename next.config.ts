8d70ee3107fc03cf1396f494359cef7ce1e173fd
Young-Nate <nathanfayulu@gmail.com>
2026-04-17 23:13:25 +0200

    Add blog URL rewrites

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['127.0.0.1', 'localhost'],
+  async rewrites() {
+    return [
+      {
+        source: '/blog',
+        destination: '/blog/index.html',
+      },
+      {
+        source: '/blog/',
+        destination: '/blog/index.html',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
